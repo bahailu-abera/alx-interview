8b8ee23b3f7b4e6d3df4d648a78d8c32901cc57d
bahailu <aberabahailu4@gmail.com>
2022-12-21 12:14:03 +0300

    change algorithm

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """
-0-pascal_triangle
+Pascal Triangle Algorithm in python3
 """
 
 
-def find(n):
+def pascal_triangle(n):
     """
-     returns a list of lists of integers representing
-     the Pascal’s triangle of n
+    Creates a pascal triangle
+    @n: level of the pascal triangle
+    Return: list of list
     """
+    result = []
     if n <= 0:
-        return []
-    triangle = []
-    row = [1]
-    triangle.append(row)
-    for i in range(1, n):
-        row = []
-        row.append(triangle[i - 1][0])
-        j = 0
-        while j < len(triangle[i - 1]) - 1:
-            row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
-            j += 1
-        row.append(triangle[i - 1][j])
-        triangle.append(row)
+        return result
 
-    return triangle
+    last = [1]
+    for i in range(1, n + 1):
+        actual = [1] * i
+        actual = [last[index - 1] + last[index] if index > 0 and
+                  index < len(actual) - 1 else 1 for index,
+                  value in enumerate(actual)]
+        last = actual
+        result.append(actual)
+    return result
