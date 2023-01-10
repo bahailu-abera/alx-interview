a5b28eb4ecb578ac51938b21b3bcfbd57bf08407
bahailu <aberabahailu4@gmail.com>
2023-01-10 13:37:15 +0300

    solution

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module minoperations
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+     Returns the fewest number of operations needed to result
+    in exactly n H characters in the file.
+    """
+    prev: int
+    written: int = 1
+    left: int = n - 1
+    minop: int = 0
+
+    while left > 0:
+        # Copy and poste operation
+        if left % written == 0:
+            minop += 2
+            prev = written
+            written *= 2
+        # Paste operation only
+        else:
+            minop += 1
+            written += prev
+        left = n - written
+    return minop
