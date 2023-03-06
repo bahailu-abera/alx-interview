044e778258fe554e0b5798cc85a63baac7e691f1
bahailu-abera <aberabahailu4@gmail.com>
2023-03-06 18:36:01 +0000

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+"""
+Module for finding the perimiter of island
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the Perimeter of the island
+    """
+    perimeter = 0
+    for row in range(1, len(grid)):
+        for col in range(1, len(grid[row])):
+            if grid[row][col]:
+                if grid[row - 1][col] == 0:
+                    perimeter += 2
+                if grid[row][col - 1] == 0:
+                    perimeter += 2
+
+    return perimeter
