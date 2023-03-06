8bdbfc11535fe221a02275ce5af223a8cb16ad9a
bahailu-abera <aberabahailu4@gmail.com>
2023-03-06 18:46:38 +0000

    handle with sides at edges

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,12 +9,12 @@ def island_perimeter(grid):
     Returns the Perimeter of the island
     """
     perimeter = 0
-    for row in range(1, len(grid)):
-        for col in range(1, len(grid[row])):
-            if grid[row][col]:
-                if grid[row - 1][col] == 0:
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 2
-                if grid[row][col - 1] == 0:
+                if col == 0 or grid[row][col - 1] == 0:
                     perimeter += 2
 
     return perimeter
