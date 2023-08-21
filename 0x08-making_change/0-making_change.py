95ddec84bbd561e4ef6715bbcfe823bbcc87dda8
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-21 20:54:30 +0300

    fix: updated

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,6 +13,7 @@ def makeChange(coins, total):
         return 0
     dp = [float('inf')] * (total+1)
     dp[0] = 0
+
     for coin in coins:
         for j in range(coin, total+1):
             dp[j] = min(dp[j], dp[j-coin]+1)
